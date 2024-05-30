b6a571b89cf38da1ad16a73d74d14a0be62a0716
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2024-05-30 10:22:23 -0400

    Added questions and safe

--- main.lua
+++ main.lua
@@ -3,14 +3,55 @@ local bump = require 'bump'
 local Final_Background = love.graphics.newImage('CalculusTheGameBackground.png')
 local Final_Character = love.graphics.newImage('lilMan.png')
 local Final_Character_Reversed = love.graphics.newImage('lilManReversed.png')
-local shouldLoadWorld = false 
-local worldLoaded = false 
+local Final_Question_1 = love.graphics.newImage('Question1.png')
+local Final_Question_2 = love.graphics.newImage('Question2.png')
+local Final_Question_3 = love.graphics.newImage('Question3.png')
+local Final_Question_4 = love.graphics.newImage('Question4.png')
+local Final_Question_5 = love.graphics.newImage('Question5.png')
+local Final_Question_6 = love.graphics.newImage('Question6.png')
+local Final_Question_7 = love.graphics.newImage('Question7.png')
+local Final_Question_8 = love.graphics.newImage('Question8.png')
+local Final_RiemannClue = love.graphics.newImage('RiemannClue.jpg')
+local shouldLoadWorld = false
+local worldLoaded = false
 local showBorders = false
 
 local isMovingLeft = false
+local shouldDrawSpeechBox = false
+local playerInteract = false
+local doorKey = false
+local safeKey = false
+local unlockedTADesk = false
+local loadSafeLock = false
+local loadTADeskLock = false
+local viewingQuestions = false
+local questionNumber = 1
+local loadRiemann = false
+
+--Password is 8050, found with Q1 2016 part B. Picture of Bernard Riemann as a clue
+local safeComboLock = {0, 0, 0, 0}
+local safeComboLockSelectedSlot = 1
+local isAtSafe = false
+
+--password is 1389, idk how to make that evident
+local TADeskComboLock = {0, 0, 0, 0}
+local TADeskComboLockSelectedSlot = 1
+local isAtTADesk = false
+
+local speechBoxLocation = 0
+local time_per_letter = .01
+local time_passed = 0
+local current_letter = 0
+local displayMessage = ""
+local doorLockedMessage = "\nThe door is locked. Maybe Ms.Holloway has hidden a key somewhere..."
+local lockedTADeskMessage = "\nThe TA desk has a drawer, but it is locked with a combination lock. Thankfully, our TA is forgetful, \n\nso he probably wrote the code down somewhere..."
+local unlockedTADeskMessage = "\nThe TA desk opens with a click. You find a few papers with calculus questions on them.\nYou can tab through these questions by using the left or right arrow key."
+local lockedSafeMessage = "\nThere is a small safe on this desk. It is locked with a 4 digit combination lock. You can press 'r' \nto view the combination lock. Press 't' to enter your guess."
+local unlockedSafeMessage = "\nThe safe opens with a click, revealing a key inside of the safe. Maybe this key would work for the room's door?" 
+local riemannMessage = "\nYou see a picture of a man with a fantastic beard attached to the chalk board."
+
 
 local world = bump.newWorld()
-
 
 
 function love.load()
@@ -28,12 +69,13 @@ end
 local function drawBox(box, r,g,b)
   love.graphics.setColor(r,g,b,0.001)
   love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
-  if showBorders == true then
-  love.graphics.setColor(r,g,b)
-  love.graphics.rectangle("line", box.x, box.y, box.w, box.h)
-end
+  if showBorders then
+  	love.graphics.setColor(r,g,b)
+  	love.graphics.rectangle("line", box.x, box.y, box.w, box.h)
+  end
   love.graphics.setColor(100,100,100,1)
 end
+  
 
 --must be above drawWorld
 local player = { x=180,y=120,w=20,h=30, speed = 200}
@@ -50,7 +92,7 @@ local function drawWorld()
     --painful part
     --Chalk board
     addBlock(215, 50, 430, 90)
-    --purple thing
+    --purple thing - Fridge?
     addBlock(75, 75, 55, 55)
     --door
     addBlock(130, 60, 85, 50)
@@ -129,8 +171,6 @@ local function drawWorld()
     --desk 5 top right thing
     addBlock(675, 320, 45, 15)
 
-
-
     worldLoaded = true
     shouldDrawWorld = true
   end
@@ -142,6 +182,8 @@ local function drawBlocks()
   end
 end
 
+--function for player movement, don't touch as it works perfectly
+--Todo: add walking animation (if time permits)
 local function updatePlayer(dt)
   local speed = player.speed
 
@@ -167,13 +209,110 @@ local function updatePlayer(dt)
 end
 
 local function drawBoxPlayer(box)
-  love.graphics.setColor(100,100,100,1)
+  love.graphics.setColor(255,255,255,1)
   if isMovingLeft == true then
   	love.graphics.draw(Final_Character_Reversed, player.x-5, player.y-15)
   elseif isMovingLeft == false then
   	love.graphics.draw(Final_Character, player.x-5, player.y-15)
   end
-  love.graphics.setColor(100,100,100,1)
+  love.graphics.setColor(255,255,255,1)
+end
+
+local function drawSpeechBox()
+  	love.graphics.rectangle('fill', 0, 600-96, 800, 96)
+   love.graphics.setColor(0,0,0,255)
+   local chars1 = displayMessage:sub(1, current_letter)
+   love.graphics.print(chars1, 50, 600-96, 0, 1, 1, 0, 0, 0, 0)
+   love.graphics.setColor(255,255,255,1)
+end
+
+local function speechUpdate(dt)
+    --Room Door
+		if player.x > 135 and player.x < 215 and player.y > 105 and player.y < 120 and playerInteract then 
+      if doorKey then
+        love.event.quit()
+      end
+			displayMessage = doorLockedMessage
+			shouldDrawSpeechBox = true
+			--TA desk
+		elseif player.x > 670 and player.x < 715 and player.y > 120 and player.y < 150 and playerInteract then
+      isAtTADesk = true
+      if unlockedTADesk then
+        displayMessage = unlockedTADeskMessage
+      elseif loadTADeskLock then
+        displayMessage = "TA Desk Combination Lock: \nSlot 1's value: " .. TADeskComboLock[1] .. ". \nSlot 2's value: " .. TADeskComboLock[2] .. ". \nSlot 3's value: " .. TADeskComboLock[3] .. ". \nSlot 4's value: " .. TADeskComboLock[4] .. ".\nSlot you are currently changing: ".. TADeskComboLockSelectedSlot
+      else
+			 displayMessage = lockedTADeskMessage
+      end
+			shouldDrawSpeechBox = true
+      --Safe Box with Room Key
+    elseif player.x > 650 and player.x < 715 and player.y > 280 and player.y < 335 and playerInteract then
+      isAtSafe = true
+      if safeKey then
+        displayMessage = unlockedSafeMessage
+      elseif loadSafeLock then
+        displayMessage = "Safe Combination Lock: \nSlot 1's value: " .. safeComboLock[1] .. ". \nSlot 2's value: " .. safeComboLock[2] .. ". \nSlot 3's value: " .. safeComboLock[3] .. ". \nSlot 4's value: " .. safeComboLock[4] .. ".\nSlot you are currently changing: ".. safeComboLockSelectedSlot
+      else
+        displayMessage = lockedSafeMessage
+      end
+      shouldDrawSpeechBox = true
+    elseif player.x > 235 and player.x < 360 and player.y > 135 and player.y < 175 and playerInteract then
+      loadRiemann = true
+      displayMessage = riemannMessage
+      shouldDrawSpeechBox = true
+		else
+			playerInteract = false
+			shouldDrawSpeechBox = false
+      isAtSafe = false
+      isAtTADesk = false
+      loadRiemann = false
+			time_passed = 0
+			current_letter = 0
+		end
+	time_passed = time_passed + dt
+    if time_passed >= time_per_letter then
+      time_passed = 0
+      current_letter = current_letter + 1
+    end
+end
+
+local function checkSafeCode()
+  if safeComboLock[1] == 8 and safeComboLock[2] == 0 and safeComboLock[3] == 5 and safeComboLock[4] == 0 then
+    doorKey = true
+  end
+end
+
+
+local function checkTADeskCode()
+  if TADeskComboLock[1] == 1 and TADeskComboLock[2] == 3 and TADeskComboLock[3] == 8 and TADeskComboLock[4] == 9 then
+    unlockedTADesk = true
+    time_passed = 0
+    current_letter = 0
+  end
+end
+
+local function loadQuestions()
+  if questionNumber == 1 then
+    love.graphics.draw(Final_Question_1, 200, 200)
+  elseif questionNumber == 2 then
+    love.graphics.draw(Final_Question_2, 200, 200)
+  elseif questionNumber == 3 then
+    love.graphics.draw(Final_Question_3, 200, 200)
+  elseif questionNumber == 4 then
+    love.graphics.draw(Final_Question_4, 200, 200)
+  elseif questionNumber == 5 then
+    love.graphics.draw(Final_Question_5, 200, 200)
+  elseif questionNumber == 6 then
+    love.graphics.draw(Final_Question_6, 200, 200)
+  elseif questionNumber == 7 then
+    love.graphics.draw(Final_Question_7, 200, 200)
+  elseif questionNumber == 8 then
+    love.graphics.draw(Final_Question_8, 200, 200)
+  end
+end
+
+local function drawRiemann() 
+  love.graphics.draw(Final_RiemannClue, 100, 100)
 end
 
 local function drawPlayer()
@@ -182,6 +321,7 @@ end
 
 function love.update(dt)
 	updatePlayer(dt)
+	speechUpdate(dt)
 end
 
 function love.draw()
@@ -192,6 +332,15 @@ function love.draw()
 	if showBorders then
 		drawBlocks()
 	end
+  if isAtTADesk and unlockedTADesk then
+    loadQuestions()
+  end
+	if shouldDrawSpeechBox and worldLoaded then
+		drawSpeechBox()
+	end
+  if loadRiemann then
+    drawRiemann()
+  end
 end
 
 function love.keypressed(k)
@@ -206,5 +355,99 @@ function love.keypressed(k)
   end
   if k=="v" then
   	showBorders = true
+  end
+  if k=="e" then 
+  	playerInteract = true
+  end
+  if k=="down" then
+    if isAtSafe then
+      if 1 + safeComboLockSelectedSlot > 4 then
+        safeComboLockSelectedSlot = 1
+      else 
+        safeComboLockSelectedSlot = 1 + safeComboLockSelectedSlot
+      end 
+    
+    elseif isAtTADesk then
+      if 1 + TADeskComboLockSelectedSlot > 4 then
+        TADeskComboLockSelectedSlot = 1
+      else 
+        TADeskComboLockSelectedSlot = 1 + TADeskComboLockSelectedSlot
+      end 
+    end
+  end
+  if k=="up" then
+    if isAtSafe then
+      if safeComboLockSelectedSlot - 1 < 1 then
+        safeComboLockSelectedSlot = 4
+      else 
+        safeComboLockSelectedSlot = safeComboLockSelectedSlot - 1
+      end
+    elseif isAtTADesk then
+      if TADeskComboLockSelectedSlot - 1 < 1 then
+        TADeskComboLockSelectedSlot = 4
+      else 
+        TADeskComboLockSelectedSlot = TADeskComboLockSelectedSlot - 1
+      end 
+    end
+  end
+  if k=="left" then
+    if isAtTADesk and unlockedTADesk then
+      if questionNumber - 1 < 1 then
+        questionNumber = 8
+      else
+        questionNumber = questionNumber - 1
+      end
+    elseif isAtSafe then
+      if safeComboLock[safeComboLockSelectedSlot] < 1 then
+        safeComboLock[safeComboLockSelectedSlot] = 9
+      else 
+        safeComboLock[safeComboLockSelectedSlot] = safeComboLock[safeComboLockSelectedSlot] - 1
+      end
+    elseif isAtTADesk then
+      if TADeskComboLock[TADeskComboLockSelectedSlot] < 1 then
+        TADeskComboLock[TADeskComboLockSelectedSlot] = 9
+      else 
+        TADeskComboLock[TADeskComboLockSelectedSlot] = TADeskComboLock[TADeskComboLockSelectedSlot] - 1
+      end
+    end
+  end
+  if k=="right" then
+    if isAtTADesk and unlockedTADesk then
+      if questionNumber + 1 > 8 then
+        questionNumber = 1
+      else
+        questionNumber = questionNumber + 1
+      end
+    elseif isAtSafe then
+      if safeComboLock[safeComboLockSelectedSlot] > 8 then
+        safeComboLock[safeComboLockSelectedSlot] = 0
+      else 
+        safeComboLock[safeComboLockSelectedSlot] = safeComboLock[safeComboLockSelectedSlot] + 1
+      end
+    elseif isAtTADesk then
+      if TADeskComboLock[TADeskComboLockSelectedSlot] > 8 then
+        TADeskComboLock[TADeskComboLockSelectedSlot] = 0
+      else 
+        TADeskComboLock[TADeskComboLockSelectedSlot] = TADeskComboLock[TADeskComboLockSelectedSlot] + 1
+      end
+    end
+  end
+  if k == "r" then 
+    if isAtSafe then
+      time_passed = 0
+      current_letter = 0
+      loadSafeLock = true
+    elseif isAtTADesk then
+      time_passed = 0
+      current_letter = 0
+      loadTADeskLock = true
+    end
+  end
+  if k == "t" then
+    if isAtSafe then
+      checkSafeCode()
+    elseif isAtTADesk then
+      checkTADeskCode()
+    end
   end
 end
